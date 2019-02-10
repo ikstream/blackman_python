3cb825d872f776dffeae7c68e1c286d24ee31a0c
Stefan Venz <stefan.venz@protonmail.com>
2019-02-10 12:25:26 +0100

    Add license to file and fix style warnings
    Signed-off-by: Stefan Venz <stefan.venz@protonmail.com>

--- blackman.py
+++ blackman.py
@@ -1,16 +1,48 @@
 #!/usr/bin/env python3
+"""
+Emerge for BlackArch Linux
+
+Download and compile packages from BlackArch github repo
+"""
+###############################################################################
+#
+#  Blackman - Emerge for BlackArch
+#  Copyright (C) 2019  Stefan Venz
+#  Copyright (C) 2019  BlackArch Linux
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+#  This is based on the original blackman
+#  <https://github.com/BlackArch/blackman>
+#
+#  Original Authors:
+#  nrz@nullsecurity.net
+#  noptrix@nullsecurity.net
+#
+###############################################################################
 
 import subprocess
 import argparse
 
 from pathlib import Path
 
-VERSION='0.1'
+VERSION = '0.1'
 
-HOME=str(Path.home())
-BLACKARCH_REPO='github'
+HOME = str(Path.home())
+BLACKARCH_REPO = 'github'
 BLACKARCH_CONFIG = HOME + '/.config/blackman/config'
-LOCAL_REPO=HOME + '/.config/blackman/repo/'
+LOCAL_REPO = HOME + '/.config/blackman/repo/'
 
 EXIT_SUCCESS = 0
 EXIT_FAILURE = 1
@@ -22,65 +54,59 @@ def print_version():
     print(VERSION)
 
 
-def check_config_dir(path = LOCAL_REPO):
+def check_config_dir():
     """
     Check if the config directory already exists
-    
+
     Arguments:
         path(str) - path for config directory
     """
-    print(path)
+    print(LOCAL_REPO)
 
 
-def create_config_dir(path = LOCAL_REPO):
+def create_config_dir():
     """
     Create config dir
-    
-    Arguments:
-        path(str) - path to config dir
     """
     try:
         err = subprocess.run(['mkdir', '-p', LOCAL_REPO], stderr=subpress.PIPE)
-        return(EXIT_SUCCESS)
+        return EXIT_SUCCESS
     except:
         print("Could not create {}".format(LOCAL_REPO))
         print(err.stderr.decode('utf-8'))
-        return(EXIT_FAILURE)
+        return EXIT_FAILURE
 
 
-def get_pkgbuild_files(path = LOCAL_REPO):
+def get_pkgbuild_files():
     """
     Clone Blackarch Repo
-    
-    Arguments:
-        path(str) - path to dir to clone into
     """
     try:
-        err = subprocess.run(['git', 'clone', BLACKARCH_REPO], stderr = subprocess.PIPE)
+        err = subprocess.run(['git', 'clone', BLACKARCH_REPO], stderr=subprocess.PIPE)
     except:
         print("Could not clone {}".format(BLACKARCH_REPO))
         print(err.stderr.decode('utf-8'))
-        return(EXIT_FAILURE)
-    return(EXIT_SUCCESS)
+        return EXIT_FAILURE
+    return EXIT_SUCCESS
 
 
-def get_dependecies(pkg, path = BLACKARCH_REPO):
+def get_dependecies(pkg, path=BLACKARCH_REPO):
     """
     Get a list of dependencies for pkg
-    
+
     Arguments:
         pkg(str) - package to be build
         path(str) - path to config
-        
+
     Return:
         dep(list) - list of deo
     """
-    pkg_build = path + 'packages/' + pkg + PKGBUILD
+    pkg_build = path + 'packages/' + pkg + 'PKGBUILD'
     try:
         with open(pkg_build):
             print('read dependencies here')
     except:
-         print("Could not open {}".format(pkg_build))
+        print("Could not open {}".format(pkg_build))
 
 
 def read_from_config():
@@ -91,7 +117,7 @@ def read_from_config():
         with open(BLACKARCH_CONFIG):
             print("read repo location from config")
     except:
-            print("Could not read from {}".format(BLACKARCH_CONFIG))
+        print("Could not read from {}".format(BLACKARCH_CONFIG))
 
 
 def write_to_config_file():
@@ -114,61 +140,82 @@ def list_groups():
     """
     ret = EXIT_SUCCESS
     try:
-        p = subprocess.run(['pacman', '-Sg', '|', 'grep', 'blackarch'], stdout=subprocess.PIPE,  stderr = subprocess.PIPE)
-        out, err = p.communicate()
+        p = subprocess.run(['pacman', '-Sg', '|', 'grep', 'blackarch'],
+                           stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        err = p.communicate()
     except:
         print("Something went wrong fetching BlackArch groups")
         print(err)
         ret = EXIT_FAILURE
-        
+
     return ret
-        
-    
-    
-    
+
+
+
+
 def parse_arguments():
     """
     parse command line options
-    
+
     return:
         args(dict) - dict of command line options
     """
     global LOCAL_REPO
     ap = argparse.ArgumentParser()
-    
+
     # package options
-    ap.add_argument("-D", "--default", required = False, help = "Change main repo location to <path>", metavar = '<path>')
-    ap.add_argument("-i", "--install", required = False, help = "install <pkg>", metavar = '<pkg>', nargs = '+')
-    ap.add_argument("-a", "--all", required = False, help = "install all packages from all groups", metavar = '', action = 'append_const', const = 1)
-    ap.add_argument("-g", "--group", required = False, help = "install all packages from <group>", metavar = '<group>',  nargs = '+')
-    ap.add_argument("-s", "--search", required = False, help = "search for <pkg>", metavar = '<pkg>')
+    ap.add_argument("-D", "--default", required=False,
+                    help="Change main repo location to <path>",
+                    metavar='<path>')
+    ap.add_argument("-i", "--install", required=False,
+                    help="install <pkg>", metavar='<pkg>', nargs='+')
+    ap.add_argument("-a", "--all", required=False,
+                    help="install all packages from all groups",
+                    metavar='', action='append_const', const=1)
+    ap.add_argument("-g", "--group", required=False,
+                    help="install all packages from <group>",
+                    metavar='<group>', nargs='+')
+    ap.add_argument("-s", "--search", required=False,
+                    help="search for <pkg>", metavar='<pkg>')
 
     # Additional options
-    ap.add_argument("-V", "--version", required = False, help = "print blackman version", metavar = '', action = 'append_const', const = 1)
-    ap.add_argument("-v", "--verbose", required = False, help = "print verbose options", metavar = '', action = 'append_const', const = 1)
-    
+    ap.add_argument("-V", "--version", required=False,
+                    help="print blackman version",
+                    metavar='', action='append_const', const=1)
+    ap.add_argument("-v", "--verbose", required=False,
+                    help="print verbose options", metavar='',
+                    action='append_const', const=1)
+
     # Repo options
-    ap.add_argument("-l", "--list", required = False, help = "list all blackarch groups", metavar = '', action = 'append_const', const = 1)
-    ap.add_argument("-p", "--packages", required = False, help = "list all packages from <group>", metavar = '<group>', nargs = '+')
+    ap.add_argument("-l", "--list", required=False,
+                    help="list all blackarch groups",
+                    metavar='', action='append_const', const=1)
+    ap.add_argument("-p", "--packages", required=False,
+                    help="list all packages from <group>",
+                    metavar='<group>', nargs='+')
     args = vars(ap.parse_args())
 
     if args["default"]:
         LOCAL_REPO = args["default"]
-    
+
     print(LOCAL_REPO)
-    return(args)
+    return args
 
 
 def handle_args(args):
     """
     Handle command line arguments
-    
+
     Arguments:
         args(dict) - command line arguments dictionary
     """
     print(args)
 
+
 def main():
+    """
+    main function of blackman
+    """
     args = parse_arguments()
     handle_args(args)
 
