a7c62f0e3a152ecc7c11e1f362405ad86dc47aca
Stefan Venz <stefan.venz@protonmail.com>
2019-05-30 18:08:32 +0200

    tests: Add more tests
    Signed-off-by: Stefan Venz <stefan.venz@protonmail.com>

--- blackman/tests/test_blackman.py
+++ blackman/tests/test_blackman.py
@@ -2,8 +2,14 @@ import unittest
 import blackman
 
 class PackageTest(unittest.TestCase):
-    def test_version(self):
+    print("Test blackman\n")
+    print(blackman.HOME)
+    def test_groups(self):
         self.assertEqual(blackman.list_groups(), 0)
+    def test_dirs(self):
+        print(f"Testing if {blackman.BLACKARCH_CONFIG} exists")
+        self.assertEqual(blackman.check_config_dir(), 0)
+
 
 if __name__ == '__main__':
     unittest.main()
--- blackman/tests/test_package.py
+++ blackman/tests/test_package.py
@@ -2,6 +2,7 @@ import unittest
 from package import package
 
 class PackageTest(unittest.TestCase):
+    print("Test package class\n")
     def test_is_in_blackarch(self):
         self.assertTrue(package.is_in_blackarch_repo('rekall'))
         self.assertFalse(package.is_in_blackarch_repo('wireshark'))
